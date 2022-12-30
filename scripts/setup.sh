44453a4f53a9d30be17fb3231ce161f9c1500c7f
MIZUNO Hiroki <mzpppp@gmail.com>
2022-12-30 09:01:50 +0000

    fix to support codespaces

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eu
 
-docker-compose run api bundle exec rails db:setup
-
-
+docker compose run frontend npm install
+docker compose run api bundle install
+docker compose run api bundle exec rails db:setup
