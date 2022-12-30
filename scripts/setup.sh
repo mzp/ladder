ae0a9921de72e53ca5adaa5e03232ef65799886b
MIZUNO Hiroki <mzpppp@gmail.com>
2022-12-30 23:58:49 +0000

    update setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eu
 
+docker compose build
 docker compose run frontend npm install
 docker compose run api bundle install
-docker compose run api bundle exec rails db:setup
+docker compose run api bundle exec rails db:setup db:migrate
