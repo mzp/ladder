d784a9f26ab54e996c6a282c7804c96c45a9730f
mzp <mzpppp@gmail.com>
2023-01-01 11:06:47 +0900

    add sidekiq

---
+++ api/app/jobs/fetch_feed_job.rb
@@ -0,0 +1,7 @@
+class FetchFeedJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+  end
+end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
+require 'sidekiq/web'
 
 Rails.application.routes.draw do
   resources 'channels', only: [:index]
-  get 'reader/unread'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  mount Sidekiq::Web => "/sidekiq"
 end
