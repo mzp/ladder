5242adeb5f60b7693a6722d24dee69da11d5e4a8
mzp <mzpppp@gmail.com>
2023-01-04 16:33:17 +0900

    fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-let apiRoot = null
+let apiRoot = 'https://api.letsrss.com'
 if (process.env.NODE_ENV == 'development') {
     if (process.env.CODESPACE_NAME != null) {
         apiRoot = `https://${process.env.CODESPACE_NAME}-3000.preview.app.github.dev`
