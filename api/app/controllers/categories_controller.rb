08548e320ecb0db22eebfe9e79befa37c0fb84dd
mzp <mzpppp@gmail.com>
2023-01-06 12:26:51 +0900

    fix category

--- api/app/controllers/categories_controller.rb
+++ api/app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class CategoriesController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index
     render json: current_user.categories.available
   end
